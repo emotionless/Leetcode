5f0f077a2da92d60dff27b96b5858b763f5d5ee9
Faruk Hossain Milon <milonju01@gmail.com>
2022-01-06 12:22:46 +0600

    Time: 16 ms (43.16%), Space: 10.7 MB (16.57%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        unordered_map<int, int> counter;
+        map<int, int> counter;
         for (auto trip : trips) {
             counter[trip[1]] += trip[0];
             counter[trip[2]] -= trip[0];
         }
         int cur = 0;
-        for (int i = 0; i <= 100000; i++) {
-            cur += counter[i];
+        for (auto it : counter) {
+            cur += it.second;
             if (cur > capacity) return false;
         }
         return true;
