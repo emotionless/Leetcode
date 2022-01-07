9a922007e0b279dc1a078828926040007d27a847
Faruk Hossain Milon <milonju01@gmail.com>
2022-01-07 19:12:58 +0600

    Time: 202 ms (8.46%), Space: 41.1 MB (21.14%) - LeetHub

---
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string makeFancyString(string s) {
+        string ret = "";
+        char prev = ' ';
+        int cnt = 0;
+        for (auto ch : s) {
+            if (ch == prev) {
+                cnt++;
+            } else {
+                cnt = 1;
+            }
+            if (cnt <= 2) {
+                ret += ch;
+            }
+            prev = ch;
+        }
+        return ret;
+    }
+};
