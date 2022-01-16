e167d846faa8794434cf73b0f9691c1edf4b65f0
Faruk Hossain Milon <milonju01@gmail.com>
2022-01-16 10:10:47 +0600

    Time: 2 ms (100.00%), Space: 5.9 MB (100.00%) - LeetHub

---
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minMoves(int target, int maxDoubles) {
+        int ans = 0;
+        while (target > 1) {
+            if (maxDoubles == 0) {
+                ans += (target - 1);
+                break;
+            }
+            if (target % 2 == 0) {
+                target /= 2;
+                maxDoubles--;
+            } else {
+                target--;
+            }
+            ans++;
+        }
+        return ans;
+    }
+};
