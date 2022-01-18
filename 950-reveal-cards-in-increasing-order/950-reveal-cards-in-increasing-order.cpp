86201ea42dc0e2c22c5526bcc44ffe13ea9add5b
Faruk Hossain Milon <milonju01@gmail.com>
2022-01-19 00:14:39 +0600

    Time: 0 ms (100.00%), Space: 8.5 MB (26.92%) - LeetHub

--- 950-reveal-cards-in-increasing-order/950-reveal-cards-in-increasing-order.cpp
+++ 950-reveal-cards-in-increasing-order/950-reveal-cards-in-increasing-order.cpp
@@ -4,18 +4,19 @@ public:
         int n = deck.size();
         vector<int> ans(n, 0);
         sort(deck.begin(), deck.end());
-        int ind = 1;
-        int cnt = 2;
+        deque<int> q;
+        for (int i = 0; i < n; i++) {
+            q.push_back(i);
+        }
         for (auto v : deck) {
-            while (cnt <= 1) {
-                if (ans[ind] == 0) {
-                    cnt++;
-                }
-                ind++;
-                ind %= n;
+            auto top = q.front();
+            q.pop_front();
+            if (q.size()) {
+                int repeat = q.front();
+                q.pop_front();
+                q.push_back(repeat);
             }
-            ans[(ind - 1 + n)%n] = v;
-            cnt = 0;
+            ans[top] = v;
         }
         return ans;
     }
