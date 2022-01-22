e6963c791e3710531305e12113f0e31788699c88
Faruk Hossain Milon <milonju01@gmail.com>
2022-01-23 00:50:57 +0600

    Time: 494 ms (14.00%), Space: 84.6 MB (23.20%) - LeetHub

--- 1690-stone-game-vii/1690-stone-game-vii.cpp
+++ 1690-stone-game-vii/1690-stone-game-vii.cpp
@@ -8,24 +8,21 @@ public:
         int &ret = dp[st][ed];
         if (ret != -1) return ret;
         ret = 0;
-        ret = csum[st + 1][ed] - solve(st + 1, ed, stones);
-        ret = max(ret, csum[st][ed-1] - solve(st, ed - 1, stones));
+        ret = csum[ed + 1] - csum[st + 1] - solve(st + 1, ed, stones);
+        ret = max(ret, csum[ed] - csum[st] - solve(st, ed - 1, stones));
         return ret;
     }
     
     int stoneGameVII(vector<int>& stones) {
         int n = stones.size();
         dp.resize(n, vector<int>(n, -1));
-        csum.resize(n, vector<int>(n, 0));
+        csum.resize(n + 1, 0);
         for (int i = 0; i < n; i++) {
-            int sum = 0;
-            for (int j = i; j < n; j++) {
-                sum += stones[j];
-                csum[i][j] = sum;
-            }
+            csum[i+1] = stones[i] + csum[i];
         }
-        return solve(0, n-1, stones);
+        return solve(0, n - 1, stones);
     }
 private:
-    vector<vector<int>> dp, csum;
+    vector<vector<int>> dp;
+    vector<int> csum;
 };
