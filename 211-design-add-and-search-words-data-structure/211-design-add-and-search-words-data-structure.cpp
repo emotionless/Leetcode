db338565434ff0488f0d3e47d4f5fee3a455e8b7
Faruk Hossain Milon <milonju01@gmail.com>
2022-01-28 11:39:18 +0600

    Time: 140 ms (36.70%), Space: 43.4 MB (68.81%) - LeetHub

---
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,59 @@
+class WordDictionary {
+public:
+    
+    struct ListNode {
+        bool isEnd;
+        ListNode *next[26];
+        ListNode() {
+            isEnd = false;
+            for (int i = 0; i < 26; i++) {
+                next[i] = nullptr;
+            }
+        }
+    };
+    
+    
+    WordDictionary() {
+        root = new ListNode();
+    }
+    
+    void addWord(string word) {
+        ListNode *cur = root;
+        for (auto ch : word) {
+            int id = ch - 'a';
+            if (cur->next[id] == nullptr) cur->next[id] = new ListNode();
+            cur = cur->next[id];
+        }
+        cur->isEnd = true;
+    }
+    
+    bool search(int ind, string &word, ListNode *cur) {
+        if (cur == nullptr) return false;
+        if (ind == word.size()) {
+            return cur->isEnd;
+        }
+        int id = word[ind] - 'a';
+        int ret = 0;
+        if (word[ind] == '.') {
+            for (int i = 0; i < 26; i++) {
+                ret += search(ind + 1, word, cur->next[i]);
+            }
+        } else {
+            ret = search(ind + 1, word, cur->next[id]);
+        }
+        return ret;
+    }
+    
+    bool search(string word) {
+        return search(0, word, root);
+    }
+private:
+    ListNode *root;
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
