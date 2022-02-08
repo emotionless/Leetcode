6ceffffbf915586ab08c1e20260d6c6cb0bf3723
Faruk Hossain Milon <milonju01@gmail.com>
2022-02-08 11:44:43 +0600

    Time: 4 ms (43.34%), Space: 5.8 MB (75.31%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int addDigits(int num) {
+        if (num == 0) return 0;
+        else if (num%9 == 0) return 9;
+        return num%9;
         if (num < 10) return num;
         int sum = 0;
         while (num) {
