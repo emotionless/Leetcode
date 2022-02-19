e50de0fcb02d0d16f71034683afcd96c97f18e04
Faruk Hossain Milon <milonju01@gmail.com>
2022-02-20 01:10:59 +0600

    Time: 635 ms (20.53%), Space: 121.2 MB (27.81%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -2,12 +2,9 @@ class Solution {
 public:
     int minimumDeviation(vector<int>& nums) {
         set<int> container;
-        int mn = INT_MAX, mx = INT_MIN;
         for (auto v : nums) {
             int num = (v&1? v*2 : v);
             container.insert(num);
-            mx = max(mx, num);
-            mn = min(mn, num);
         }
         int ans = *container.rbegin() - *container.rend();
         while (*container.rbegin() % 2 == 0) {
