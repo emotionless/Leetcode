d5ff709e4cf05bbfa1913100b673dec0d619db47
Faruk Hossain Milon <milonju01@gmail.com>
2022-03-05 11:53:28 +0600

    Time: 10 ms (61.81%), Space: 9.9 MB (61.62%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -2,16 +2,20 @@ class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
         unordered_map<int, int> value;
+        int mx = 0;
+        int mn = 100000;
         for (auto v : nums) {
             value[v] += v;
+            mx = max(mx, v);
+            mn = min(mn, v);
         }
-        vector<int> dp(10003, 0);
-        for (int i = 10000; i >= 0; i--) {
+        vector<int> dp(mx + 3, 0);
+        for (int i = mx; i >= mn; i--) {
             int mx = value[i];
             mx = dp[i+2] + value[i];
             mx = max(mx, dp[i+1]);
             dp[i] = mx;
         }
-        return dp[0];
+        return dp[mn];
     }
 };
