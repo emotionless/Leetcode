7ed3512b0b3879144df24081d24dcf5700de77e2
Faruk Hossain Milon <milonju01@gmail.com>
2022-03-05 11:52:13 +0600

    Time: 168 ms (5.00%), Space: 41.8 MB (5.02%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -5,16 +5,13 @@ public:
         for (auto v : nums) {
             value[v] += v;
         }
-        set<int> s(nums.begin(), nums.end());
-        vector<int> arr(s.begin(), s.end());
-        int n = arr.size();
-        vector<int> dp(arr[n-1] + 3, 0);
-        for (int i = arr[n - 1]; i >= 0; i--) {
+        vector<int> dp(10003, 0);
+        for (int i = 10000; i >= 0; i--) {
             int mx = value[i];
             mx = dp[i+2] + value[i];
             mx = max(mx, dp[i+1]);
             dp[i] = mx;
         }
-        return dp[arr[0]];
+        return dp[0];
     }
 };
