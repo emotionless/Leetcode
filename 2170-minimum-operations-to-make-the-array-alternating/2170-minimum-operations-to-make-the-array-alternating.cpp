5f5be4b9fcafde216cc147bcd5d012f201f3a1e6
Faruk Hossain Milon <milonju01@gmail.com>
2022-03-10 15:26:17 +0600

    Time: 420 ms (72.61%), Space: 133.6 MB (84.64%) - LeetHub

---
+++ 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    
+    struct comparator {
+        bool operator()(const pair<int, int> &a, const pair<int, int> &b) {
+            if (a.second == b.second) -a.second < -b.second;
+            return -a.first < -b.first;
+        }
+    };
+    
+    pair<pair<int, int>, pair<int, int>> getTwoLargeNumber(unordered_map<int, int> &counter) {
+        pair<int, int> first({0, 0}), second({0, 0});
+        for (auto ele : counter) {
+            if (ele.second > first.second) {
+                second = first;
+                first = make_pair(ele.first, ele.second);
+            } else if (ele.second >= second.second) {
+                second = make_pair(ele.first, ele.second);
+            }
+        }
+        return {first, second};
+    }
+    
+    int minimumOperations(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, int> counter;
+        for (int i = 0; i < n; i+=2) {
+            counter[nums[i]]++;
+        }
+        auto firstTwo = getTwoLargeNumber(counter);
+        counter.clear();
+        for (int i = 1; i < n; i+=2) {
+            counter[nums[i]]++;
+        }
+        auto secondTwo = getTwoLargeNumber(counter);
+        
+        int ans = n;
+        if (firstTwo.first.first != secondTwo.first.first) {
+            ans = min(ans, n - firstTwo.first.second - secondTwo.first.second);
+        }
+        if (firstTwo.first.first != secondTwo.second.first) {
+            ans = min(ans, n - firstTwo.first.second - secondTwo.second.second);
+        }
+        if (firstTwo.second.first != secondTwo.first.first) {
+            ans = min(ans, n - firstTwo.second.second - secondTwo.first.second);
+        }
+        if (firstTwo.second.first != secondTwo.second.first) {
+            ans = min(ans, n - firstTwo.second.second - secondTwo.second.second);
+        }
+        return ans;
+    }
+};
