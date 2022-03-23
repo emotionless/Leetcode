94a928f44a188a148c39a110ee9841066f2b1be0
Faruk Hossain Milon <milonju01@gmail.com>
2022-03-23 11:05:30 +0600

    Time: 0 ms (100.00%), Space: 6 MB (30.21%) - LeetHub

---
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        int steps = 0;
+        while (target > startValue) {
+            steps++;
+            if (target&1) target++;
+            else target /= 2;
+        }
+        return steps + startValue - target;
+    }
+};
