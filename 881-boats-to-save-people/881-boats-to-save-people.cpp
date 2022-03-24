af99bc2ca6c85d3aef14b70cc35f709bfa5e6f23
Faruk Hossain Milon <milonju01@gmail.com>
2022-03-24 11:19:38 +0600

    Time: 76 ms (87.76%), Space: 41.9 MB (57.42%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -4,14 +4,14 @@ public:
         sort(people.begin(), people.end());
         int n = people.size();
         int st = 0, ed = n - 1;
-        int counter = 0;
+        int ans = 0;
         while (st <= ed) {
             if ((people[ed] + people[st]) <= limit) {
                 st++;
             }
             ed--;
-            counter++;
+            ans++;
         }
-        return counter;
+        return ans;
     }
 };
