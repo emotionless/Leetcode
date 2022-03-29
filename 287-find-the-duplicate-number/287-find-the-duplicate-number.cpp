e4046d27175caca5e752cb57c96c116e238618ed
Faruk Hossain Milon <milonju01@gmail.com>
2022-03-29 13:41:54 +0600

    Time: 112 ms (87.34%), Space: 61.2 MB (61.36%) - LeetHub

---
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        int st = 1, ed = n - 1;
+        int ans = -1;
+        while (st < ed) {
+            int mid = (st + ed) / 2;
+            int cnt = 0;
+            for (auto &v : nums) {
+                if (v <= mid) {
+                    cnt++;
+                }
+            }
+            if (cnt <= mid) st = mid + 1;
+            else ed = mid;
+        }
+        return st;
+    }
+};
