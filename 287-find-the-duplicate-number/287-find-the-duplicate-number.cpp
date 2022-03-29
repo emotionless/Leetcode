730caf5cf2c8fe636e2c69173107ad51444d2288
Faruk Hossain Milon <milonju01@gmail.com>
2022-03-29 14:51:16 +0600

    Time: 166 ms (51.94%), Space: 61.3 MB (26.65%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -5,7 +5,7 @@ public:
         int sum = 0;
         int st = 1, ed = n - 1;
         int ans = -1;
-        while (st < ed) {
+        while (st <= ed) {
             int mid = (st + ed) / 2;
             int cnt = 0;
             for (auto &v : nums) {
@@ -14,8 +14,11 @@ public:
                 }
             }
             if (cnt <= mid) st = mid + 1;
-            else ed = mid;
+            else {
+                ans = mid;
+                ed = mid - 1;
+            }
         }
-        return st;
+        return ans;
     }
 };
