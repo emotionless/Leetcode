2e2f9f3fa43d57a56ba05e9947caecfd7074661d
Faruk Hossain Milon <milonju01@gmail.com>
2022-04-03 01:46:16 +0600

    Time: 80 ms (58.66%), Space: 10 MB (73.05%) - LeetHub

--- 2212-maximum-points-in-an-archery-competition/2212-maximum-points-in-an-archery-competition.cpp
+++ 2212-maximum-points-in-an-archery-competition/2212-maximum-points-in-an-archery-competition.cpp
@@ -1,36 +1,6 @@
 class Solution {
 public:
     
-    int solve(int numArrows, int ind, int mask, vector<int> &aliceArrows) {
-        if (ind >= 12) {
-            return 0;
-        }
-        int &ret = dp[mask][ind];
-        if (ret != -1) return ret;
-        ret = 0;
-        ret = solve(numArrows, ind + 1, mask, aliceArrows);
-        path[mask][ind] = 0;
-        if (aliceArrows[ind] < numArrows) {
-            int now = ind + solve(numArrows - aliceArrows[ind] - 1, ind + 1, mask | (1<<ind), aliceArrows);
-            if (now > ret) {
-                ret = now;
-                path[mask][ind] = aliceArrows[ind] + 1;
-            }
-        }
-        
-        return ret;
-    }
-    
-    void dfs(int numArrows, int ind, int mask, vector<int> &ans) {
-        if (ind >= 12) {
-            return;
-        }
-        ans.push_back(path[mask][ind]);
-        dfs(numArrows - path[mask][ind], ind + 1, (path[mask][ind]?mask|(1<<ind) : mask), ans);
-        
-        return;
-    }
-    
     vector<int> maximumBobPoints(int numArrows, vector<int>& aliceArrows) {
         int n = aliceArrows.size();
         int ans = 0;
@@ -62,6 +32,4 @@ public:
         
         return ret;
     }
-private:
-    vector<vector<int>> dp, path;
 };
