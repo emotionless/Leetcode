7e61f1f66318d16ecf2e7fe980131fc8a29c5254
Faruk Hossain Milon <milonju01@gmail.com>
2022-04-11 01:15:11 +0600

    Time: 153 ms (78.17%), Space: 57.4 MB (76.22%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.cpp
+++ 1472-design-browser-history/1472-design-browser-history.cpp
@@ -1,12 +1,12 @@
 class BrowserHistory {
 public:
-    BrowserHistory(const string &homepage) {
+    BrowserHistory(string homepage) {
         container.clear();
         container.push_back(homepage);
         index = 0;
     }
     
-    void visit(const string &url) {
+    void visit(string url) {
         if ((index+1) == container.size()) {
             container.push_back(url);
             index++;
