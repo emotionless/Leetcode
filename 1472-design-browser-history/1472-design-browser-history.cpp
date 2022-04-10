02ef7b8af7152c869908964030d49b005a92b454
Faruk Hossain Milon <milonju01@gmail.com>
2022-04-11 01:13:45 +0600

    Time: 128 ms (92.59%), Space: 57.2 MB (83.45%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.cpp
@@ -0,0 +1,42 @@
+class BrowserHistory {
+public:
+    BrowserHistory(string homepage) {
+        container.clear();
+        container.push_back(homepage);
+        index = 0;
+    }
+    
+    void visit(string url) {
+        if ((index+1) == container.size()) {
+            container.push_back(url);
+            index++;
+        } else {
+            container[++index] = url;
+            container.resize(index + 1);
+        }
+    }
+    
+    string back(int steps) {
+        index -= steps;
+        index = max(index, 0);
+        return container[index];
+    }
+    
+    string forward(int steps) {
+        index += steps;
+        index = min(index, (int)container.size() - 1);
+        
+        return container[index];
+    }
+private:
+    int index = 0;
+    vector<string> container;
+};
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory* obj = new BrowserHistory(homepage);
+ * obj->visit(url);
+ * string param_2 = obj->back(steps);
+ * string param_3 = obj->forward(steps);
+ */
