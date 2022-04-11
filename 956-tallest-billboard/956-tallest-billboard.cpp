8e54b4c59433a92c1191d2da3ab9efbdc647d3c8
Faruk Hossain Milon <milonju01@gmail.com>
2022-04-12 01:27:01 +0600

    Time: 117 ms (48.21%), Space: 45.1 MB (40.62%) - LeetHub

--- 956-tallest-billboard/956-tallest-billboard.cpp
+++ 956-tallest-billboard/956-tallest-billboard.cpp
@@ -3,7 +3,7 @@ public:
     const int INF = 1e6;
     const int MID = 2500;
     
-    int solve(int ind, int sum, int &ans, vector<int> &rods) {
+    int solve(int ind, int sum, vector<int> &rods) {
         if (sum < 0 || sum > 2*MID) return -INF;
         if (ind == n) {
             if (sum == MID) {
@@ -16,10 +16,10 @@ public:
         int &ret = dp[ind][sum];
         if (ret != -1) return ret;
         ret = 0;
-        ret = solve(ind + 1, sum, ans, rods);
+        ret = solve(ind + 1, sum, rods);
         
-        int ret1 = rods[ind] + solve(ind + 1, sum + rods[ind], ans, rods);
-        int ret2 = rods[ind] + solve(ind + 1, sum - rods[ind], ans, rods);
+        int ret1 = rods[ind] + solve(ind + 1, sum + rods[ind], rods);
+        int ret2 = rods[ind] + solve(ind + 1, sum - rods[ind], rods);
         ret = max(ret, ret1);
         ret = max(ret, ret2);
         return ret;
@@ -29,8 +29,8 @@ public:
         n = rods.size();
         int midSum = accumulate(rods.begin(), rods.end(), 0) / 2;
         dp.resize(n + 1, vector<int>(2*MID + 1, -1));
-        int ans = 0;
-        return solve(0, MID, ans, rods)/2;
+        
+        return solve(0, MID, rods)/2;
     }
 private:
     int n;
