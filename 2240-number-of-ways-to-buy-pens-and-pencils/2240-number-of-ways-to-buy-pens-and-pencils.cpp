9dcdb0a3366c60f958d96eba8e2171bf3dffba00
Faruk Hossain Milon <milonju01@gmail.com>
2022-04-18 16:43:14 +0600

    Time: 20 ms (100.00%), Space: 6 MB (100.00%) - LeetHub

--- 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.cpp
+++ 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     long long waysToBuyPensPencils(int total, int cost1, int cost2) {
-        long long res = total / cost2 + 1;
-        for (int i = cost1; i <= total; i += cost1) {
-            int now = (total - i) / cost2;
-            res += now + 1;
+        long long res = 0;
+        for (int i = 0; i <= total; i += cost1) {
+            res += (total - i) / cost2 + 1;
         }
         return res;
     }
