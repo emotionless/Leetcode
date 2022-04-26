b91e0af09224fa978e0bdb42263d1ab5f53620d6
Faruk Hossain Milon <milonju01@gmail.com>
2022-04-27 01:29:20 +0600

    Time: 164 ms (89.87%), Space: 13.7 MB (92.51%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -2,56 +2,31 @@ class Solution {
 public:
     
     struct node {
-        int x, y, c;
-        node (int _, int __, int ___) {
-            x = _;
-            y = __;
-            c = ___;
+        int ind, c;
+        node (int _, int __) {
+            ind = _;
+            c = __;
         }
         bool operator<(const node &a) const {
             return c > a.c;
         }
     };
     
-    // vector<int> par;
-    
-    // int find(int x) {
-    //     if (par[x] == x) return x;
-    //     return par[x] = find(par[x]);
-    // }
-    
-    int cost[1001];
-    
     int minCostConnectPoints(vector<vector<int>>& points) {
         priority_queue<node> pq;
         int n = points.size();
-        // par.resize(n);
-        
-        // for(int i = 0; i < n; i++) {
-        //     par[i] = i;
-        //     for(int j = i + 1; j < n; j++) {
-        //         pq.push(node(i, j, abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])));
-        //     }
-        // }
-        pq.push(node(0, 0, 0));
+        pq.push(node(0, 0));
         int ans = 0;
-        bool vis[n];
-        memset(vis, false, sizeof vis);
-        for(int i = 0; i < n; i++) {
-            cost[i] = INT_MAX;
-        }
+        vector<bool> vis(n, false);
+        vector<int> cost(n, INT_MAX);
+        
         while(!pq.empty()) {
             node top = pq.top();
             pq.pop();
-            int x = top.x;
-            int y = top.y;
+            int y = top.ind;
             int c = top.c;
-            
-            // int px = find(x);
-            // int py = find(y);
             if (vis[y]) continue;
             vis[y] = true;
-            // par[px] = py;
             ans += c;
             
             for(int i = 0; i < n; i++) {
@@ -59,7 +34,7 @@ public:
                     int cst = abs(points[i][0] - points[y][0]) + abs(points[i][1] - points[y][1]);
                     if (cst < cost[i]) {
                         cost[i] = cst;
-                        pq.push(node(y, i, cst));
+                        pq.push(node(i, cst));
                     }
                 }
             }
