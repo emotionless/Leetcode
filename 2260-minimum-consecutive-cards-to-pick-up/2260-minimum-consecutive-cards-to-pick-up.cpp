abb65525152a32189c3ebb7785cf0310af7fb788
Faruk Hossain Milon <milonju01@gmail.com>
2022-05-10 12:06:38 +0600

    Time: 454 ms (66.66%), Space: 115.1 MB (74.76%) - LeetHub

---
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumCardPickup(vector<int>& cards) {
+        unordered_map<int, int> pos;
+        int n = cards.size();
+        int ans = n + 2;
+        for (int i = 0; i < n; i++) {
+            if (pos.find(cards[i]) != pos.end()) {
+                ans = min(ans, i - pos[cards[i]]);
+            } 
+            pos[cards[i]] = i;
+        }
+        if (ans > n) return -1;
+        return ans + 1;
+    }
+};
