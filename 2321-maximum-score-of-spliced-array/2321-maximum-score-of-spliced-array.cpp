03838ec6023ba5f9c9d1b77e7bda1f02233bb9a3
Faruk Hossain Milon <milonju01@gmail.com>
2022-06-27 14:06:32 +0600

    Time: 145 ms (66.67%), Space: 82.5 MB (100.00%) - LeetHub

--- 2321-maximum-score-of-spliced-array/2321-maximum-score-of-spliced-array.cpp
+++ 2321-maximum-score-of-spliced-array/2321-maximum-score-of-spliced-array.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
     
-    int getMaxSum(const vector<int> &arr) {
-        int n = arr.size();
-        int sum = 0;
-        int mx = 0;
-        for (auto &num : arr) {
-            sum += num;
-            if (sum < 0) sum = 0;
-            mx = max(mx, sum);
-        }
-        return mx;
-    }
-    
     int maximumsSplicedArray(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         int sum1 = accumulate(nums1.begin(), nums1.end(), 0);
         int sum2 = accumulate(nums2.begin(), nums2.end(), 0);
         int ans = max(sum1, sum2);
-        vector<int> arr;
+        int sum = 0;
+        int cur = 0;
         for (int i = 0; i < n; i++) {
-            arr.push_back(nums1[i] - nums2[i]);
+            int num = (nums1[i] - nums2[i]);
+            sum += num;
+            if (sum < 0) sum = 0;
+            cur = max(cur, sum);
         }
-        ans = max(ans, sum2 + getMaxSum(arr));
-        arr.clear();
+        ans = max(ans, sum2 + cur);
+        sum = 0;
+        cur = 0;
         for (int i = 0; i < n; i++) {
-            arr.push_back(nums2[i] - nums1[i]);
+            int num = (nums2[i] - nums1[i]);
+            sum += num;
+            if (sum < 0) sum = 0;
+            cur = max(cur, sum);
         }
-        ans = max(ans, sum1 + getMaxSum(arr));
+        ans = max(ans, sum1 + cur);
         
         return ans;
     }
