56dde6e7ee10fc3c13036305cf4219f7f341fe74
Faruk Hossain Milon <milonju01@gmail.com>
2022-06-27 14:13:18 +0600

    Time: 227 ms (66.67%), Space: 82.5 MB (100.00%) - LeetHub

--- 2321-maximum-score-of-spliced-array/2321-maximum-score-of-spliced-array.cpp
+++ 2321-maximum-score-of-spliced-array/2321-maximum-score-of-spliced-array.cpp
@@ -3,12 +3,14 @@ public:
     
     int maximumsSplicedArray(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
-        int num1Sum = accumulate(nums1.begin(), nums1.end(), 0);
-        int num2Sum = accumulate(nums2.begin(), nums2.end(), 0);
+        int num1Sum = 0;
+        int num2Sum = 0;
         int ans = max(num1Sum, num2Sum);
         int sum1 = 0, sum2 = 0;
         int cur1 = 0, cur2 = 0;
         for (int i = 0; i < n; i++) {
+            num1Sum += nums1[i];
+            num2Sum += nums2[i];
             sum1 += (nums1[i] - nums2[i]);
             sum2 += (nums2[i] - nums1[i]);
             if (sum1 < 0) sum1 = 0;
