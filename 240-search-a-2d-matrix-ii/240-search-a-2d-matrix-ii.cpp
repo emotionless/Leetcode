2192ea563baa46594e79cc6ffbb72bdbfd74a8c5
Faruk Hossain Milon <milonju01@gmail.com>
2022-07-25 03:38:48 +0600

    Time: 147 ms (65.66%), Space: 15 MB (21.86%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+    
+    
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        if(n == 0) return false;
+        int m = matrix[0].size();
+        
+        int i = n-1, j = 0;
+        while(i  >= 0 && j < m) {
+            if(matrix[i][j] == target) return true;
+            else if(matrix[i][j] > target) {
+                i--;
+            } else {
+                j++;
+            }
+        }
+        
+        return false;
+        
+    }
+};
