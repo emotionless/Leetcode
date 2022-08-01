c40551cca3739f9572d82ac66c89d9197634f8ec
Faruk Hossain Milon <milonju01@gmail.com>
2022-08-01 15:28:32 +0600

    Time: 87 ms (100.00%), Space: 56.6 MB (100.00%) - LeetHub

--- 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
+++ 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int maximumGroups(vector<int>& grades) {
         int n = grades.size();
+        return sqrt(2*n + 0.25f) - 0.5f;
         int ans = 0;
         for (int i = 1; n >= i; i++) {
             ans = i;
