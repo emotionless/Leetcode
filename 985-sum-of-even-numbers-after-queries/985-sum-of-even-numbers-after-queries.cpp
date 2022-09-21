b0478e3fad30006447b19d1763e629b9acf6bf10
Faruk Hossain Milon <milonju01@gmail.com>
2022-09-21 13:43:30 +0600

    Time: 102 ms (98.61%), Space: 46.1 MB (38.69%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
@@ -5,16 +5,14 @@ public:
         for (auto &num : nums) {
             sum += num&1?0:num;
         }
-        int n = nums.size();
-        int cur = 0;
-        vector<int> ans(n);
+        vector<int> ans;
         for (auto &q : queries) {
             int val = q[0];
             int ind = q[1];
             if (nums[ind]%2==0) sum -= nums[ind];
             nums[ind] += val;
             if (nums[ind]%2==0) sum += nums[ind];
-            ans[cur++] = sum;
+            ans.push_back(sum);
         }
         return ans;
     }
