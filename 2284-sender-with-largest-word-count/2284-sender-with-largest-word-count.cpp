272aae2a247fa93395a9a08715a83582df1c5cec
Faruk Hossain Milon <milonju01@gmail.com>
2022-10-23 14:41:06 +0600

    Time: 517 ms (42.29%), Space: 72 MB (67.07%) - LeetHub

--- 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.cpp
+++ 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.cpp
@@ -5,17 +5,17 @@ public:
         unordered_map<string, int> messageCount;
         int n = senders.size();
         int mx = 0;
-        string ans = "";
+        int ans = -1;
         for (int i = 0; i < n; i++) {
             messageCount[senders[i]] += count(begin(messages[i]), end(messages[i]), ' ') + 1;
             int cnt = messageCount[senders[i]];
-            if (cnt == mx) {
-                ans = max(ans, senders[i]);
+            if (cnt == mx && senders[i] > senders[ans]) {
+                ans = i;
             } else if (cnt > mx) {
-                ans = senders[i];
+                ans = i;
                 mx = cnt;
             }
         }
-        return ans;
+        return senders[ans];
     }
 };
