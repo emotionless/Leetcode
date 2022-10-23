720defd365939e6736e0784a71fc3991027b3a69
Faruk Hossain Milon <milonju01@gmail.com>
2022-10-24 04:43:17 +0600

    Time: 147 ms (91.91%), Space: 71.4 MB (29.92%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -30,7 +30,7 @@ public:
         long long ans = 0;
         for (int i = 0; i < n; i++) {
             sum += nums[i];
-            ans = max(ans, sum/(i+1) + (sum%(i+1)?1:0));
+            ans = max(ans, (sum + i)/(i+1));
         }
         
         return ans;
