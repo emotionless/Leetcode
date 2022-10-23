d2fcb37ffe4ce915b4e65601bffbf4caeb6675b2
Faruk Hossain Milon <milonju01@gmail.com>
2022-10-24 04:30:16 +0600

    Time: 234 ms (76.77%), Space: 71.3 MB (86.67%) - LeetHub

---
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    
+    bool possible(vector<int> &nums, const int &tar) {
+        int n = nums.size();
+        long long sum = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            sum += tar - nums[i];
+            if (sum > 0) sum = 0;
+        }
+        return sum >= 0;
+    }
+    
+    int minimizeArrayValue(vector<int>& nums) {
+        int n = nums.size();
+        int st = 0, ed = *max_element(nums.begin(), nums.end());
+        int ans = ed;
+        while (st <= ed) {
+            int mid = (st + ed) / 2;
+            if (possible(nums, mid)) {
+                ans = mid;
+                ed = mid - 1;
+            } else {
+                st = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
