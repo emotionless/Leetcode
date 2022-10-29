d5fefd706f75848369b8861ba5b9420257accd36
Faruk Hossain Milon <milonju01@gmail.com>
2022-10-29 09:47:49 +0600

    Time: 468 ms (78.51%), Space: 168.7 MB (29.92%) - LeetHub

---
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    struct node {
+        int plant, grow;
+        node(int _plant, int _grow) {
+            plant = _plant;
+            grow = _grow;
+        }
+    };
+    
+    struct comparator {
+        bool operator() (const node &A, const node &B) {
+            int sumA = A.plant + A.grow;
+            int sumB = B.plant + B.grow;
+            if (B.grow == A.grow) {
+                return A.plant > B.plant;
+            }
+            return A.grow < B.grow;
+        }
+    };
+    
+    int earliestFullBloom(vector<int>& plantTime, vector<int>& growTime) {
+        int n = plantTime.size();
+        priority_queue<node, vector<node>, comparator> pq;
+        for (int i = 0; i < n; i++) {
+            pq.push(node(plantTime[i], growTime[i]));
+        }
+        int mx = 0;
+        int sum = 0;
+        while (!pq.empty()) {
+            auto top = pq.top();
+            pq.pop();
+            // cout << top.plant << " " << top.grow << endl;
+            sum += top.plant;
+            mx = max(mx, sum + top.grow);
+        }
+        return mx;
+    }
+};
