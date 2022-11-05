e106b7ac45147adb0fe0ab4e8a4d17d0cdb01b46
Faruk Hossain Milon <milonju01@gmail.com>
2022-11-05 13:43:31 +0600

    Time: 570 ms (16.48%), Space: 78.5 MB (98.34%) - LeetHub

--- 2449-minimum-number-of-operations-to-make-arrays-similar/2449-minimum-number-of-operations-to-make-arrays-similar.cpp
+++ 2449-minimum-number-of-operations-to-make-arrays-similar/2449-minimum-number-of-operations-to-make-arrays-similar.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    long long numberOfSteps(vector<int> &list1, vector<int> &list2) {
-        sort(list1.begin(), list1.end());
-        sort(list2.begin(), list2.end());
-        int n = list1.size();
-        int carry = 0;
-        long long steps = 0;
-        for (int i = 0; i < n; i++) {
-            int diff = abs(list1[i] - list2[i]);
-            steps += diff;
+    struct comparator {
+        bool operator() (const int &num1, const int &num2) {
+            if (num1%2 == num2%2) return num1 < num2;
+            return num1%2 < num2%2;
         }
-        return steps/2;
-    }
+    };
+    
     long long makeSimilar(vector<int>& nums, vector<int>& target) {
-        vector<int> odd1, even1;
-        vector<int> odd2, even2;
-        for (auto &num : nums) {
-            if (num&1) odd1.push_back(num);
-            else even1.push_back(num);
+        sort(nums.begin(), nums.end(), comparator());
+        sort(target.begin(), target.end(), comparator());
+        int n = nums.size();
+        long long tot = 0;
+        for (int i = 0; i < n; i++) {
+            tot += abs(nums[i] - target[i]);
         }
-        for (auto &num : target) {
-            if (num&1) odd2.push_back(num);
-            else even2.push_back(num);
-        }
-        return (numberOfSteps(odd1, odd2) + numberOfSteps(even1, even2))/2;
+        return tot/4;
     }
 };
