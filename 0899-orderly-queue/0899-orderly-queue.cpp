7083b220bf983174b082fd07a496bc715b202643
Faruk Hossain Milon <milonju01@gmail.com>
2022-11-06 12:22:31 +0600

    Time: 5 ms (75.86%), Space: 10.8 MB (32.18%) - LeetHub

---
+++ 0899-orderly-queue/0899-orderly-queue.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string orderlyQueue(string s, int k) {
+        if(k == 1) {
+            string ans = s;
+            for(int i = 0; i < s.size(); i++) {
+                ans = min(ans, (s.substr(i) + s.substr(0, i)));
+            }
+            return ans;
+        } else {
+            sort(s.begin(), s.end());
+            return s;
+        }
+    }
+};
