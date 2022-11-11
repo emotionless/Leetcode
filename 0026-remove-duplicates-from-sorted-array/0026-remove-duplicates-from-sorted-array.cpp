0477e17c7221e857be9c01462db9b00ea2e03915
Faruk Hossain Milon <milonju01@gmail.com>
2022-11-11 12:55:25 +0600

    Time: 23 ms (61.75%), Space: 18.4 MB (74.88%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 0) return 0;
+        int j = 0;
+        for(int i = 1; i < n; i++) {
+            if (nums[i] != nums[j]) {
+                nums[++j] = nums[i];
+            }
+        }
+        return j + 1;
+    }
+};
