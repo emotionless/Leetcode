639139af0e3ceff6990a4b1d3a687976ee6a5d8b
Faruk Hossain Milon <milonju01@gmail.com>
2023-02-04 13:02:24 +0600

    Time: 17 ms (43.98%), Space: 8.4 MB (21.90%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool checkInclusion(string p, string s) {
+        int n = s.length();
+        
+        int cnt[n + 1][26];
+        int tar[26];
+        memset(cnt, 0, sizeof cnt);
+        memset(tar, 0, sizeof tar);
+        int plen = p.length();
+        
+        for(int i = 0; i < plen; i++) {
+            tar[ p[i] - 'a' ]++;
+        }
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < 26; j++) {
+                if (i)
+                    cnt[i][j] = cnt[i-1][j];
+            }
+            cnt[i][s[i] - 'a']++;
+            if (i < (plen-1)) continue;
+            bool ck = false;
+            for(int j = 0; j < 26; j++) {
+                int have = cnt[i][j] - ((i-plen)>=0?cnt[i-plen][j] : 0);
+                if (have != tar[j]) { ck = true; break; }
+            }
+            if (ck == false) return true;
+        }
+        return false;
+    }
+};
