9a793f27a5b8ad9342277301c6639d2094ee6a45
Faruk Hossain Milon <milonju01@gmail.com>
2023-07-26 21:07:43 -0700

    Time: 773 ms (16.04%), Space: 228.7 MB (9.73%) - LeetHub

--- 2718-sum-of-matrix-after-queries/2718-sum-of-matrix-after-queries.cpp
+++ 2718-sum-of-matrix-after-queries/2718-sum-of-matrix-after-queries.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     long long matrixSumQueries(int n, vector<vector<int>>& queries) {
-        int row = 0, col = 0;
         long long ans = 0ll;
         unordered_set<int> rows, cols;
         for (int i = queries.size() - 1; i >= 0; i--) {
@@ -9,13 +8,11 @@ public:
             if (query[0] == 0) {
                 if (rows.find(query[1]) != rows.end()) continue;
                 rows.insert(query[1]);
-                ans += query[2] * (n - col);
-                row++;
+                ans += query[2] * (n - cols.size());
             } else {
                 if (cols.find(query[1]) != cols.end()) continue;
                 cols.insert(query[1]);
-                ans += query[2] * (n - row);
-                col++;
+                ans += query[2] * (n - rows.size());
             }
         }
         return ans;
