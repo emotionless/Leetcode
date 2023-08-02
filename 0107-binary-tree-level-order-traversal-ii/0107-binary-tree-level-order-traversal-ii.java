3e4def2b15fb5a9a67851b3b3bdc40bc3c575a31
Faruk Hossain Milon <milonju01@gmail.com>
2023-08-01 17:08:40 -0700

    Time: 1 ms (94.64%), Space: 41.4 MB (99.57%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
@@ -14,25 +14,23 @@
  * }
  */
 class Solution {
-    
-    private void traverse(TreeNode node, int level, Map<Integer, List<Integer>> hashMap) {
-        if (node == null) return;
-        if (hashMap.get(level) == null) {
-            hashMap.put(level, new ArrayList<Integer>());
-        }
-        hashMap.get(level).add(node.val);
-        traverse(node.left, level + 1, hashMap);
-        traverse(node.right, level + 1, hashMap);
-    }
-    
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        Map<Integer, List<Integer>> hashMap = new HashMap<>();
-        traverse(root, 0, hashMap);
-        List<Integer> keys = new ArrayList<Integer>(hashMap.keySet());
-        List<List<Integer>> ans = new ArrayList<>();
-        for (int i = keys.size() - 1; i >= 0; i--) {
-            ans.add(hashMap.get(keys.get(i)));
+        List<List<Integer>> treeOrder = new ArrayList<>();
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root == null) return treeOrder;
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            int sz = queue.size();
+            List<Integer> subList = new ArrayList<>();
+            for (int i = 0; i < sz; i++) {
+                if (queue.peek().left != null) queue.offer(queue.peek().left);
+                if (queue.peek().right != null) queue.offer(queue.peek().right);
+                subList.add(queue.poll().val);
+            }
+            treeOrder.add(0, subList);
         }
-        return ans;
+        return treeOrder;
     }
 }
