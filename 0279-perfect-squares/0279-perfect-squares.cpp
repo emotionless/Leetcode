cfa8bd3ca2c5749e816792fbe81c5c903c99b810
Faruk Hossain Milon <milonju01@gmail.com>
2024-02-08 11:41:01 +0800

    Time: 32 ms (94.44%), Space: 10.5 MB (38.33%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int numSquares(int n) {
-        vector<int> dp(n + 1, INT_MAX);
-        dp[0] = 0;
+        vector<int> dp(n + 1, 0);
+        dp[1] = 1;
         for (int i = 1; i <= n; i++) {
+            dp[i] = n;
             for (int j = 1; j*j <= i; j++) {
-                dp[i] = min(dp[i], 1 + dp[i - j*j]);
+                dp[i] = min(dp[i], dp[i - j*j] + 1);
             }
         }
         return dp[n];
