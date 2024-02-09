f1f7688327454686960c76626969a6d016512be2
Faruk Hossain Milon <milonju01@gmail.com>
2024-02-09 12:38:08 +0800

    Time: 14 ms (75.44%), Space: 43.5 MB (16.43%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<Integer> largestDivisibleSubset(int[] nums) {
-        List<Pair<Integer, Integer>> list = new ArrayList<Pair<Integer, Integer>>();
+        List<Integer> list = new ArrayList<>();
         Arrays.sort(nums);
         int ans = 0;
         int pos = 0;
@@ -8,8 +8,8 @@ class Solution {
             int mx = 1, ind = i;
             for (int j = i - 1; j >= 0; j--) {
                 if (nums[i] % nums[j] == 0) {
-                    if ((list.get(j).getKey() + 1) > mx) {
-                        mx = list.get(j).getKey() + 1;
+                    if ((list.get(j) + 1) > mx) {
+                        mx = list.get(j) + 1;
                         ind = j;
                     }
                 }
@@ -18,14 +18,17 @@ class Solution {
                 ans = mx;
                 pos = i;
             }
-            list.add(new Pair(mx, ind));
+            list.add(mx);
         }
         List<Integer> res = new ArrayList<>();
-        res.add(nums[pos]);
-        while (pos != list.get(pos).getValue()) {
-            pos = list.get(pos).getValue();
-            res.add(nums[pos]);
-        } 
+        int num = nums[pos];
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (num % nums[i] == 0 && list.get(i) == ans) {
+                res.add(nums[i]);
+                num = nums[i];
+                ans--;
+            }
+        }
         
         return res;
     }
