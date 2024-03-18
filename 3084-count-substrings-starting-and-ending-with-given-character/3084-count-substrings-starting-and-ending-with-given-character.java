e1ae0a44a1762c2b65957a4e5a2467470e08052e
Faruk Hossain Milon <milonju01@gmail.com>
2024-03-18 13:12:48 +0800

    Time: 2 ms (100.00%), Space: 44.6 MB (100.00%) - LeetHub

--- 3084-count-substrings-starting-and-ending-with-given-character/3084-count-substrings-starting-and-ending-with-given-character.java
+++ 3084-count-substrings-starting-and-ending-with-given-character/3084-count-substrings-starting-and-ending-with-given-character.java
@@ -1,8 +1,7 @@
 class Solution {
     public long countSubstrings(String s, char c) {
         long cnt = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
+        for (char ch : s.toCharArray()) {
             if (c == ch) {
                 cnt++;
             }
