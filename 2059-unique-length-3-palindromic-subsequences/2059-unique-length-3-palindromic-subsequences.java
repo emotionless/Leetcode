327bf2db6a3bb11dc8487e01d1df9aee68aa7161
Faruk Hossain Milon <milonju01@gmail.com>
2025-01-04 11:34:56 +0800

    Time: 51 ms (84.82%), Space: 45.3 MB (71.2%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -1,6 +1,6 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-        int[] myHashSet = new int[2630];
+        boolean[] myHashSet = new boolean[2630];
         int[] counter = new int[26];
         int[] counter2 = new int[26];
         int len = s.length();
@@ -13,15 +13,15 @@ class Solution {
             for (int j = 0; j < 26; j++) {
                 if (counter2[j] != 0 && counter[j] != 0){
                     int num = j*100 + (s.charAt(i) - 'a');
-                    myHashSet[num] = 1;
+                    myHashSet[num] = true;
                 }
             }
             
             counter2[(int)(s.charAt(i) - 'a')]++;
         }
         int ans = 0;
-        for (int i : myHashSet) {
-            ans += i;
+        for (boolean i : myHashSet) {
+            ans += i == true? 1:0;
         }
         return ans;
     }
