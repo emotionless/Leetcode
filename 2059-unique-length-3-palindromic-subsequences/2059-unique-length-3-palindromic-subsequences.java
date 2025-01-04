051323f4e4bc9132f72c6e4efd2da2bdc93faad0
Faruk Hossain Milon <milonju01@gmail.com>
2025-01-04 11:27:04 +0800

    Time: 309 ms (18.06%), Space: 45.3 MB (71.2%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -3,13 +3,14 @@ class Solution {
         HashSet<Integer> myHashSet = new HashSet<Integer>();
         int[] counter = new int[26];
         int[] counter2 = new int[26];
+        int len = s.length();
         for (int i = 0; i < 26; i++) {
             counter[i] = counter2[i] = 0;
         }
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < len; i++) {
             counter[(int)(s.charAt(i) - 'a')]++;
         }
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < len; i++) {
             counter[(int)(s.charAt(i) - 'a')]--;
             for (int j = 0; j < 26; j++) {
                 if (counter2[j] != 0 && counter[j] != 0){
