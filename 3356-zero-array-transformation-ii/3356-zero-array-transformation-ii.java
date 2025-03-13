dc951ab2e5f68cd4632cc52ad09bbea665be8c5f
Faruk Hossain Milon <milonju01@gmail.com>
2025-03-13 22:26:46 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    boolean doesApplyZero(int[] nums, int[][] queries, int k) {
+        int n = nums.length;
+        int[] sum = new int[n + 1];
+        for (int i = 0; i <= k; i++) {
+            int l = queries[i][0];
+            int r = queries[i][1];
+            sum[l] -= queries[i][2];
+            sum[r + 1] += queries[i][2];
+        }
+        long csum = 0;
+        for (int i = 0; i < n; i++) {
+            csum += sum[i];
+            if (csum + nums[i] > 0) return false; 
+        }
+        return true;
+    }
+
+    public int minZeroArray(int[] nums, int[][] queries) {
+        int st = 0, ed = queries.length - 1;
+        int ans = -2;
+        while (st <= ed) {
+            int mid = (st + ed) / 2;
+            if (doesApplyZero(nums, queries, mid)) {
+                ans = mid;
+                ed = mid - 1;
+            } else {
+                st = mid + 1;
+            }
+        }
+        return ans + 1;
+    }
+}
