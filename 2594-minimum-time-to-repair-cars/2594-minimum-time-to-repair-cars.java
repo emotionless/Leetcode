c266f0a0925349e81cd03fb8fb1f393dbb5c90e1
Faruk Hossain Milon <milonju01@gmail.com>
2025-03-16 20:36:41 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.java
@@ -0,0 +1,28 @@
+class Solution {
+    long carRepaired(long tar, int[] ranks) {
+        int n = ranks.length;
+        long tot = 0;
+        for (int rank : ranks) {
+            long div = tar / rank;
+            long sq = (long)Math.sqrt(div);
+            tot += sq;
+        }
+        return tot;
+    }
+    public long repairCars(int[] ranks, int cars) {
+        long st = 1, ed = cars*cars *101;
+        long ans = ed;
+        while (st <= ed) {
+            long mid = (st + ed) / 2;
+            long tot = carRepaired(mid, ranks);
+            System.out.println(mid + " " + tot);
+            if (tot >= cars) {
+                ans = mid;
+                ed = mid - 1;
+            } else {
+                st = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
