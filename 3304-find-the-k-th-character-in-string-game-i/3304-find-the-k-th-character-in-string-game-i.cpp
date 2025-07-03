e8eeb279fa3ae969469eed62d2029fa5845141e6
Faruk Hossain Milon <milonju01@gmail.com>
2025-07-03 22:26:32 +0800

    Time: 0 ms (100%), Space: 7.9 MB (90.95%) - LeetHub

--- 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.cpp
+++ 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     int power[30] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912};
 
-    int getPrePos(int cur) {
-        int last = 0;
+    int getPrePos(int &cur) {
         for (int i = 0; i < 30; i++) {
             if (power[i] >= cur) {
+                return cur - power[i-1];
                 break;
             }
-            last = power[i];
         }
-        return cur - last;
+        return 0;
     }
 
     char kthCharacter(int k) {
