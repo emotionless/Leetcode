3edc9e8351ded3fae370c52a8b46a73068dc20cc
Faruk Hossain Milon <milonju01@gmail.com>
2025-07-11 01:11:57 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3440-reschedule-meetings-for-maximum-free-time-ii/3440-reschedule-meetings-for-maximum-free-time-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxFreeTime(int eventTime, vector<int>& startTime, vector<int>& endTime) {
+        int last = 0;
+        int k = 1;
+        int n = startTime.size();
+        vector<int> container(n + 2, 0);
+        for (int i = 0; i < n; i++) {
+            container[i] = (startTime[i] - last);
+            last = endTime[i];
+        }
+        container[n] = (eventTime - last);
+        container[n+1] = 0;
+
+        int sz = n + 2;
+        vector<int> mx(sz, 0);
+        mx[sz - 1] =  container[sz - 1];
+        for (int i =  sz - 2; i >= 0; i--) {
+            mx[i] = max(container[i], mx[i+1]);
+            // cout << mx[i] << " ";
+        }
+        int leftMx = 0;
+        int ans = 0;
+        for (int i = 0; i < sz - 2; i++) {
+            int rightMx = mx[i+2];  // check validation
+            cout << leftMx << " " << (endTime[i] - startTime[i]) << " " << rightMx << endl;
+            if (max(leftMx, rightMx) >= (endTime[i] - startTime[i])) {
+                ans = max(ans, container[i] + container[i+1] +  (endTime[i] - startTime[i]));
+            } else  {
+                ans = max(ans, container[i] + container[i+1]);
+            }
+            leftMx = max(leftMx, container[i]);
+        }
+        return ans;
+    }
+};
