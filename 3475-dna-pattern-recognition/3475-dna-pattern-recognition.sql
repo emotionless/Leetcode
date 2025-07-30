1db8e7bc385af5a5ff2d0503b67c933f30be21ad
Faruk Hossain Milon <milonju01@gmail.com>
2025-07-30 23:46:18 +0800

    Time: 292 ms (47.57%), Space: 0B (100%) - LeetHub

---
+++ 3475-dna-pattern-recognition/3475-dna-pattern-recognition.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+SELECT sample_id, dna_sequence, species,
+CASE
+    WHEN dna_sequence LIKE 'ATG%' THEN 1
+    ELSE 0
+END AS has_start,
+CASE
+    WHEN dna_sequence LIKE '%TAA' OR dna_sequence LIKE '%TAG' OR dna_sequence LIKE '%TGA' THEN 1
+    ELSE 0
+END AS has_stop,
+CASE
+    WHEN dna_sequence LIKE '%ATAT%' THEN 1
+    ELSE 0
+END AS has_atat,
+CASE
+    WHEN dna_sequence LIKE '%GGG%' THEN 1
+    ELSE 0
+END AS has_ggg
+FROM Samples;
