04f092abc06625afa4796cf58c4951e120d0754e
Faruk Hossain Milon <milonju01@gmail.com>
2025-08-03 09:59:54 +0800

    Time: 95 ms (28.94%), Space: 173.3 MB (22.13%) - LeetHub

---
+++ 2106-maximum-fruits-harvested-after-at-most-k-steps/2106-maximum-fruits-harvested-after-at-most-k-steps.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k) {
+        int n = fruits.size();
+        int mx = max(startPos, fruits[n-1][0]);
+        vector<int> csum(mx  +  1, 0);
+        for (auto fruit : fruits) {
+            csum[fruit[0]] = fruit[1];
+        }
+        for (int i = 1; i <= mx; i++) {
+            csum[i] += csum[i-1]; 
+            // cout << i << " " << csum[i] <<  endl;
+        }
+        cout << endl;
+        int ans = 0;
+        for (int i = max(0, startPos - k); i <= startPos; i++) {
+            int cur = getCumulativeSum(i, startPos, csum);
+            int remaining = k - 2*(startPos - i);
+            if (remaining >  0) {
+                cur = getCumulativeSum(i, startPos + remaining, csum);
+            }
+            // cout << i << " " << cur << endl;
+            ans = max(ans, cur);
+        }
+        for (int i = startPos + 1; i <= startPos + k; i++) {
+            int cur = getCumulativeSum(startPos, i, csum);
+            int remaining = k - 2*(i - startPos);
+            if (remaining >  0) {
+                cur = getCumulativeSum(startPos - remaining, i, csum);
+            }
+            // cout << i << " " << cur << endl;
+            ans = max(ans, cur);
+        }
+        return ans;
+    }
+private:
+    int getCumulativeSum(int st, int ed, vector<int> &csum) {
+        // cout<< st << " " <<  ed << " " << csum.size() << endl;
+        ed = min(ed, (int)csum.size()-1);
+        if (st > 0) {
+            return csum[ed] - csum[st - 1];
+        } else {
+            return csum[ed];
+        }
+    }
+};
