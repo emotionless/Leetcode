3eb2dc2ddc190a8ddc2e066d4227267fc6c3589d
Faruk Hossain Milon <milonju01@gmail.com>
2025-08-04 11:32:47 +0800

    Time: 426 ms (91.03%), Space: 0B (100%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -1,8 +1,12 @@
 # Write your MySQL query statement below
-SELECT id,
+WITH RANKED AS (
+    SELECT DISTINCT(p_id) FROM Tree WHERE p_id IS NOT NULL
+)
+SELECT t.id,
     CASE
-        WHEN  p_id is NULL THEN 'Root'
-        WHEN id IN (SELECT DISTINCT(p_id) FROM Tree WHERE p_id IS NOT NULL) THEN  'Inner'
+        WHEN  t.p_id is NULL THEN 'Root'
+        WHEN r.p_id is NOT NULL  THEN  'Inner'
         ELSE 'Leaf'
     END AS type
-FROM Tree;
+FROM Tree t
+LEFT JOIN RANKED r ON t.id = r.p_id
