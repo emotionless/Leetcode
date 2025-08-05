c9e9e5b564ee3af11012f61e8c4f231b75c52ee4
Faruk Hossain Milon <milonju01@gmail.com>
2025-08-05 22:49:53 +0800

    Time: 309 ms (37.08%), Space: 176.1 MB (97.75%) - LeetHub

---
+++ 3479-fruits-into-baskets-iii/3479-fruits-into-baskets-iii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+
+    void markVisited(int block, int fruit, vector<int> &baskets) {
+        int n = baskets.size();
+        int sq = sqrt(n) + 1;
+        int st = block*sq, ed = (block+1)*sq;
+        for (int j =  st; j < ed && j < n; j++) {
+            if (baskets[j] >= fruit) {
+                baskets[j] = 0;
+                break;
+            }
+        }
+        values[block] = 0;
+        for (int j = st; j < ed && j < n; j++) {
+            values[block] = max(values[block], baskets[j]);
+        }
+        return;
+    }
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int n = baskets.size();
+        int sq = sqrt(n) + 1;
+        values.resize(sq + 1,  0);
+        for (int i = 0; i < n; i++) {
+            int block = i / sq;
+            values[block] = max(values[block], baskets[i]);
+        }
+        int found = 0;
+        for (auto fruit : fruits) {
+            for (int i = 0; i <= sq; i++) {
+                if (values[i] >= fruit) {
+                    markVisited(i, fruit, baskets);
+                    found++;
+                    break;
+                }
+            }
+        }
+        return n - found;
+    }
+private:
+    vector<int> values;
+};
