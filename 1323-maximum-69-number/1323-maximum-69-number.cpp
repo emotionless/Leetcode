5ec3b0e3ca86e8f8499dfdd2636aaa263edd77ad
Faruk Hossain Milon <milonju01@gmail.com>
2025-08-16 10:56:54 +0800

    Time: 0 ms (100%), Space: 8.2 MB (12.18%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -3,14 +3,14 @@ public:
     int maximum69Number (int num) {
         stringstream ss;
         ss << num;
-        string str;
-        ss >> str;
-        for (auto &ch : str) {
-            if (ch == '6') {
+        string strNum;
+        ss >> strNum;
+        for (auto &ch : strNum) {
+            if  (ch == '6') {
                 ch = '9';
                 break;
             }
         }
-        return stoi(str);
+        return stoi(strNum);
     }
 };
