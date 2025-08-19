086c201b49c4704dca7e1dec9bc2caf83de2d4af
Faruk Hossain Milon <milonju01@gmail.com>
2025-08-19 09:03:04 +0800

    Time: 0 ms (100%), Space: 111.4 MB (40.61%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     long long zeroFilledSubarray(vector<int>& nums) {
-        long long ans = 0;
         long long cnt = 0;
-        for (auto &num : nums) {
-            if (num) {
-                ans += (cnt*(cnt+1))/2;
+        long long ans =  0;
+        for (auto num : nums) {
+            if (num == 0) cnt++;
+            else {
+                ans += (cnt*(cnt+1ll))/2ll;
                 cnt = 0;
-            } else cnt++;
+            }
         }
-        ans += (cnt*(cnt+1))/2;
+        ans += (cnt*(cnt+1ll))/2ll;
+
         return ans;
     }
 };
