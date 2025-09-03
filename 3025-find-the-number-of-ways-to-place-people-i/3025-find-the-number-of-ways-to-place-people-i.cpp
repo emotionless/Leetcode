4a5d391d367c20d4f5debfd6f903d519a60c6c93
Faruk Hossain Milon <milonju01@gmail.com>
2025-09-03 16:40:18 +0800

    Time: 0 ms (100%), Space: 32.6 MB (72.46%) - LeetHub

--- 3025-find-the-number-of-ways-to-place-people-i/3025-find-the-number-of-ways-to-place-people-i.cpp
+++ 3025-find-the-number-of-ways-to-place-people-i/3025-find-the-number-of-ways-to-place-people-i.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     int numberOfPairs(vector<vector<int>>& points) {
+        sort(points.begin(), points.end(), [](const auto &lft, const auto &rgt) {
+            if (lft[0] == rgt[0]) return lft[1] > rgt[1];
+            return lft[0] < rgt[0];
+        });
         int n = points.size();
-        sort(points.begin(), points.end());
-        int ans = 0;
+        int cnt = 0;
         for (int i = 0; i < n; i++) {
-            for (int  j = 0; j < n; j++) {
-                if (i == j || points[i][0] > points[j][0] || points[i][1] < points[j][1]) continue;
-                bool ck = false;
-                for (int k = 0; k < n; k++) {
-                    if (k != i && k != j) {
-                        if (points[k][0] >= points[i][0] && points[k][0] <= points[j][0] 
-                        && points[k][1] >= points[j][1] && points[k][1] <= points[i][1]) {
-                            ck = true;
-                            break;
-                        }
-                    }
-                }
-                if (ck == false) {
-                    ans++;
+            int mx = INT_MIN;
+            for (int j = i + 1; j < n; j++) {
+                if (points[j][1] <= points[i][1] && points[j][1] > mx) {
+                    mx = points[j][1];
+                    cnt++;
                 }
             }
         }
-        return ans;
+        return cnt;
     }
 };
