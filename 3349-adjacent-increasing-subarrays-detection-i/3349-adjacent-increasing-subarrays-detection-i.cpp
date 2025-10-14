b21dced096d4463c622ac56b683bc076f688cecd
Faruk Hossain Milon <milonju01@gmail.com>
2025-10-14 23:25:26 +0800

    Time: 12 ms (68.58%), Space: 40.4 MB (75.22%) - LeetHub

--- 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.cpp
+++ 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     bool hasIncreasingSubarrays(vector<int>& nums, int k) {
         int n = nums.size();
-        if (k == 1) return true;
-        vector<int> right(n, 1);
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i+1]) right[i] = right[i+1] + 1;
-            else
-                right[i] = 1;
-        }
+        int ans = 0;
+        int percent = 0;
         int cnt = 1;
-        for (int i = 1;  i < n-1; i++) {
+        for (int i = 1;  i < n; i++) {
             if (nums[i] > nums[i-1]) cnt++;
-            else cnt = 1;
-            if (cnt >= k && right[i+1] >= k) return true;
+            else {
+                percent = cnt;
+                cnt = 1;
+            }
+            ans = max(ans, min(cnt, percent));
+            ans = max(ans, cnt / 2);
         }
-        return false;
+        return ans >= k;
         
     }
 };
