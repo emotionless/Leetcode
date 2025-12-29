8dc18042962e3c1ba75530f4a2d785e3c3aa00d6
Faruk Hossain Milon <milonju01@gmail.com>
2025-12-29 21:39:03 +0800

    Time: 2237 ms (12.44%), Space: 12.1 MB (76.76%) - LeetHub

---
+++ 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+  bool pyramidTransition(string bottom, vector<string>& allowed) {
+    unordered_map<string, vector<char>> m;
+    for(auto& s:allowed) m[s.substr(0, 2)].push_back(s.back());
+    return helper(bottom, m, 0, "");
+  }
+  bool helper(string bottom, unordered_map<string, vector<char>>& m, int start, string next){
+    if(bottom.size() == 1) return true;
+    if(start == (int)bottom.size() - 1) return helper(next, m, 0, "");
+    for(char c : m[bottom.substr(start, 2)])
+      if(helper(bottom, m, start+1, next+c)) return true;
+    return false;
+  }
+};
