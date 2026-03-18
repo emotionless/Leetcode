d00451ecadacc4c1024cd99a09f32c5682721536
Faruk Hossain Milon <milonju01@gmail.com>
2026-03-18 21:15:25 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
@@ -4,12 +4,12 @@ public:
         int n = grid.size();
         int m = grid[0].size();
 
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        vector<vector<int>> dp(2, vector<int>(m+1, 0));
         int cnt = 0;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + grid[i-1][j-1];
-                if (dp[i][j] <= k) cnt++;
+                dp[i&1][j] = dp[(i-1)&1][j] + dp[i&1][j-1] - dp[(i-1)&1][j-1] + grid[i-1][j-1];
+                if (dp[i&1][j] <= k) cnt++;
                 else break;
             }
         }
