2612973773b37cb42d23b5e0569df2a5c8ae3998
Faruk Hossain Milon <milonju01@gmail.com>
2026-04-07 22:46:17 +0800

    Time: 52 ms (21.55%), Space: 127.3 MB (93.1%) - LeetHub

---
+++ 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.cpp
@@ -0,0 +1,59 @@
+class Robot {
+public:
+    Robot(int width, int height) {
+        h =  height;
+        w = width;
+        x = y = 0;
+        dir  = 0;
+    }
+    
+    void step(int num) {
+        while (num > 0) {
+            int adjx = x + dirx[dir];
+            int adjy = y + diry[dir];
+            if (adjx < 0 || adjx >= w || adjy < 0 || adjy >= h) {
+                dir++;
+                dir = dir % 4;
+                num %= 2*(h + w - 2);
+                if (num == 0) {
+                    dir = (dir - 1 + 4) % 4;
+                }
+                continue;
+            }
+            num--;
+            x = adjx;
+            y = adjy;
+        }
+    }
+    
+    vector<int> getPos() {
+        return {x, y};
+    }
+    
+    string getDir() {
+        switch(dir) {
+            case 0:
+                return "East";
+            case 1:
+                return "North";
+            case  2:
+                return "West";
+            default:
+                return "South";
+        }
+    }
+private:
+    int h, w;
+    int x, y;
+    int dirx[4] = {1, 0,  -1, 0};
+    int diry[4] = {0,  1,  0, -1};
+    int dir = 0;
+};
+
+/**
+ * Your Robot object will be instantiated and called as such:
+ * Robot* obj = new Robot(width, height);
+ * obj->step(num);
+ * vector<int> param_2 = obj->getPos();
+ * string param_3 = obj->getDir();
+ */
