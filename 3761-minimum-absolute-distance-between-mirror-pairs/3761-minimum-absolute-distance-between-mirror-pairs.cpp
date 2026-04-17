8e2b95a49b136b312d2aebbda178044e5d6b82e4
Faruk Hossain Milon <milonju01@gmail.com>
2026-04-17 22:37:25 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.cpp
+++ 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.cpp
@@ -10,20 +10,15 @@ public:
     }
     int minMirrorPairDistance(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int, stack<int>> store;
-        for(int i = n - 1;  i  >= 0; i--) {
-            store[nums[i]].push(i);
-        }
+        unordered_map<int, int> store;
+        
         int ans = INT_MAX;
         for  (int  i =  0; i < n; i++) {
             int num = reverseNumber(nums[i]);
-            auto &cur = store[num];
-            while (!cur.empty()  && cur.top() <= i) {
-                cur.pop();
+            if (store.find(nums[i]) != store.end()) {
+                ans = min(ans, i - store[nums[i]]);
             }
-            if (!cur.empty()) {
-                ans  =  min(ans, cur.top() - i);
-            }
+            store[num] = i;
         }
         return  ans  == INT_MAX? -1 : ans;
     }
