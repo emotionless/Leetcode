60ed0ea17b38526d7f11ca5a04a4df665f3dad76
Faruk Hossain Milon <milonju01@gmail.com>
2026-06-08 23:48:14 +0800

    Time: 299 ms (17.97%), Space: 301.7 MB (14.83%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -11,32 +11,30 @@
  */
 class Solution {
 public:
-    
-    TreeNode* getHash(int v) {
-        if (hashMap.find(v) != hashMap.end()) return hashMap[v];
-        return hashMap[v] = new TreeNode(v);
-    }
-    
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
-        unordered_set<int> vis;
+        unordered_map<int, TreeNode*> hashMap;
+        unordered_map<int, bool> isRoot;
         for (auto v : descriptions) {
-            vis.insert(v[1]); 
+            TreeNode *par = NULL;
+            if (hashMap.find(v[0])  != hashMap.end()) par = hashMap[v[0]];
+            else par = new TreeNode(v[0]);
+
+            TreeNode *child = NULL;
+            if (hashMap.find(v[1]) != hashMap.end()) child = hashMap[v[1]];
+            else child = new TreeNode(v[1]);
+
+            hashMap[v[0]] = par;
+            hashMap[v[1]] = child;
+
+            if(v[2]) par->left = child;
+            else par->right=child;
+
+            isRoot[v[1]] = false;
         }
-        int val = 0;
         for (auto v : descriptions) {
-            TreeNode* from = getHash(v[0]);
-            TreeNode* to = getHash(v[1]);
-            if (vis.find(v[0]) == vis.end()) val = v[0];
-            if (vis.find(v[1]) == vis.end()) val = v[1];
-            
-            if (v[2]) {
-                from->left = to;
-            } else {
-                from->right = to;
-            }
+            if (isRoot.find(v[0]) == isRoot.end()) return hashMap[v[0]];
         }
-        return hashMap[val];
+        return NULL;
     }
-private:
-    map<int, TreeNode*> hashMap;
+
 };
